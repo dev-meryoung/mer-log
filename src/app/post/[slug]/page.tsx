a7c4e6ba2664d9161dd3b019272380fb519d73b3
meryoung <dev.meryoung@gmail.com>
2025-02-27 15:22:25 +0900

    feat: Comments 컴포넌트 구현 및 적용 #26

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import Comments from '@/components/Comments';
 import IndexNavigation from '@/components/IndexNavigation';
-import { extractHeadings, getPost, processHeadings } from '@/lib/posts';
+import { getPost, processHeadings } from '@/lib/posts';
 import { formatDate } from '@/utils/dateUtils';
 
 interface PostPageProps {
@@ -13,40 +15,45 @@ const PostPage = async ({ params }: PostPageProps) => {
   const { headings, updatedHtml } = processHeadings(contentHtml);
 
   return (
-    <article className='w-full mx-auto p-10 rounded-lg bg-white shadow-md dark:bg-darkActive'>
-      <div className='relative'>
-        <IndexNavigation headings={headings} />
-        <div>
-          <p className='text-2xl md:text-3xl lg:text-4xl font-bold mb-4 pl-0.5 dark:text-text-dark'>
-            {metadata.title}
-          </p>
-          <p className='text-sm md:text-base text-gray-500 mb-4 pl-1'>
-            {formatDate(metadata.date)}
-          </p>
-          <ul className='pb-4 mb-4 border-b border-gray-200 dark:border-text-light'>
-            {metadata.tags.map((tag) => (
-              <li key={tag} className='inline-block leading-9 mr-2'>
-                <span className='bg-gray-200 rounded-3xl px-2.5 py-1 text-sm text-gray-70 dark:bg-gray-500 dark:text-text-dark'>
-                  {tag}
-                </span>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className='relative w-full aspect-video mb-8'>
-          <Image
-            src={metadata.thumbnail}
-            alt={metadata.title}
-            fill
-            className='rounded-lg object-cover'
+    <>
+      <article className='w-full mx-auto p-10 rounded-lg bg-white shadow-md dark:bg-darkActive'>
+        <div className='relative'>
+          <IndexNavigation headings={headings} />
+          <div>
+            <p className='text-2xl md:text-3xl lg:text-4xl font-bold mb-4 pl-0.5 dark:text-text-dark'>
+              {metadata.title}
+            </p>
+            <p className='text-sm md:text-base text-gray-500 mb-4 pl-1'>
+              {formatDate(metadata.date)}
+            </p>
+            <ul className='pb-4 mb-4 border-b border-gray-200 dark:border-text-light'>
+              {metadata.tags.map((tag) => (
+                <li key={tag} className='inline-block leading-9 mr-2'>
+                  <Link href={`/?tags=${tag}`} key={tag}>
+                    <span className='bg-gray-200 rounded-3xl px-2.5 py-1 text-sm text-gray-70 dark:bg-gray-500 dark:text-text-dark'>
+                      {tag}
+                    </span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className='relative w-full aspect-video mb-8'>
+            <Image
+              src={metadata.thumbnail}
+              alt={metadata.title}
+              fill
+              className='rounded-lg object-cover'
+            />
+          </div>
+          <div
+            className='prose dark:prose-dark max-w-none'
+            dangerouslySetInnerHTML={{ __html: updatedHtml }}
           />
         </div>
-        <div
-          className='prose dark:prose-dark max-w-none'
-          dangerouslySetInnerHTML={{ __html: updatedHtml }}
-        />
-      </div>
-    </article>
+      </article>
+      <Comments />
+    </>
   );
 };
 
