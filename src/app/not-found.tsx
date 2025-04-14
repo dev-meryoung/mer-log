deabe05dc6f6a3e157e3b7944da65eadee9532ee
meryoung <dev.meryoung@gmail.com>
2025-04-14 11:53:01 +0900

    design: 404 페이지 디자인 일부 수정 #86

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,10 +2,10 @@ import Link from 'next/link';
 
 const NotFound = () => (
   <div className='w-full flex flex-col items-center px-10 py-60 gap-1.5 rounded-lg select-none bg-white shadow-md dark:bg-darkActive'>
-    <h1 className='font-recipekorea text-7xl md:text-8xl dark:text-text-dark'>
+    <h1 className='font-recipekorea text-7xl md:text-8xl text-primary dark:text-text-dark'>
       404
     </h1>
-    <h2 className='font-recipekorea text-xl md:text-2xl dark:text-text-dark'>
+    <h2 className='font-recipekorea text-xl md:text-2xl text-primary dark:text-text-dark'>
       페이지를 찾을 수 없습니다.
     </h2>
     <div className='text-sm md:text-base text-gray-600 dark:text-gray-300'>
@@ -14,7 +14,7 @@ const NotFound = () => (
     </div>
     <Link
       href='/'
-      className='text-sm md:text-base px-2.5 py-1.5 mt-2.5 rounded-md border-2 border-text-light dark:border-text-dark dark:text-text-dark'
+      className='text-sm md:text-base px-2.5 py-1.5 mt-2.5 rounded-md border-2 text-primary border-primary dark:border-text-dark dark:text-text-dark'
     >
       메인으로 이동
     </Link>
