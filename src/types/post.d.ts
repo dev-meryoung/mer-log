777808065c1466527080e91ee0d183ac9421d9e7
meryoung <dev.meryoung@gmail.com>
2026-01-22 14:35:50 +0900

    refactor: 빌드 스크립트 TS 마이그레이션 및 코드 구조 정리 #121

---
+++ src/types/post.d.ts
@@ -0,0 +1,37 @@
+import { ReactElement } from 'react';
+import { MDXRemoteProps } from 'next-mdx-remote/rsc';
+
+export interface Heading {
+  id: string;
+  text: string;
+  level: number;
+}
+
+export interface PostMeta {
+  title: string;
+  description: string;
+  date: string;
+  tags: string[];
+  thumbnail: string;
+  slug: string;
+  blurDataURL: string;
+  summary: string;
+}
+
+export type PostInfo = PostMeta & { content?: string };
+
+export type PostSearchData = Pick<
+  PostInfo,
+  'title' | 'description' | 'tags' | 'slug' | 'date'
+> & {
+  content: string;
+};
+
+export interface PostData {
+  postInfo: PostInfo;
+  mdxSource: ReactElement<MDXRemoteProps>;
+  headings: Heading[];
+  summary: string;
+  prevPost?: PostInfo | null;
+  nextPost?: PostInfo | null;
+}
